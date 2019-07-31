efb39f4d73c141f494ba8329f9c837ef7f03c136
Paul Breakthrough <paulbreakthru@outlook.com>
2019-07-31 10:40:11 +0100

    Moved product class to product.cs

---
+++ ExceptionContinue/ExceptionContinue/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExceptionContinue
+{
+    class Product
+    {
+        private int _quantity;
+        private float _price;
+        private double _sales;
+        private string _productName;
+
+        public Product()
+        {
+            _productName = "Motherboard";
+        }
+
+        public void AcceptDetails()
+        {
+            Console.Write("Enter product quantity : ");
+            try
+            {
+                _quantity = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error : {0}",ex.Message);
+            }
+
+            Console.Write("Enter the price : ");
+            if (float.TryParse(Console.ReadLine(), out _price))
+            {
+                _sales = _price * _quantity;
+            }
+            else
+                Console.WriteLine("Invalid price inserted");         
+        }
+        public void Display()
+        {
+            Console.WriteLine("Product Name : {0}",_productName);
+            Console.WriteLine("Product Quantity : {0}",_quantity);
+            Console.WriteLine("Product Price : {0}",_price);
+            Console.WriteLine("Product Sale : {0}",_sales);
+        }
+        public static void ProductTest()
+        {
+            var obj = new Product();
+            obj.AcceptDetails();
+        }
+    }
+}
