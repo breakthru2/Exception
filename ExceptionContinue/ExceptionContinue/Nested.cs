95d3e0131099d3ff16bb214947c531eb100f1a64
Paul Breakthrough <paulbreakthru@outlook.com>
2019-07-31 11:05:05 +0100

    Nested tryand multiple catch blocks

---
+++ ExceptionContinue/ExceptionContinue/Nested.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExceptionContinue
+{
+    class Nested
+    {
+        public void Nesting()
+        {
+            Console.Write("Enter Y/N");
+            try
+            {
+                char value = Char.Parse(Console.ReadLine());
+                Console.Write("Enter 1 for a, 2 for b");
+                try
+                {
+                    int option = Int32.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Error : {0}",ex.Message);
+                }
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error : {0}",ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : {0}", ex.Message);
+            }
+        }
+    }
+}
